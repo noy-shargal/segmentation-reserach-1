7e141ee9bb7405116fa4a7ed6771f1e8458bf8b9
noy shargal <noy.shargal@gmail.com>
2023-11-25 14:46:21 +0200

    first segmentation with SAM

--- main.py
+++ main.py
@@ -25,6 +25,29 @@ def print_resources_info():
     print("CUDA is_available:", torch.cuda.is_available())
 
 
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white',
+               linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white',
+               linewidth=1.25)
+
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     sys.path.append("..")
@@ -41,15 +64,36 @@ if __name__ == '__main__':
     sam = sam_model_registry[model_type](model_checkpoints)
     sam.to(device=device)
 
-    mask_generator = SamAutomaticMaskGenerator(model=sam, points_per_side=32, pred_iou_thresh=0.9,
-                                               stability_score_thresh=0.96, crop_n_layers=1, crop_n_points_downscale_factor=2,
-                                               min_mask_region_area=100) # requires openCV to run post processing
+    # mask_generator = SamAutomaticMaskGenerator(model=sam, points_per_side=32, pred_iou_thresh=0.9,
+    #                                            stability_score_thresh=0.96, crop_n_layers=1, crop_n_points_downscale_factor=2,
+    #                                            min_mask_region_area=100) # requires openCV to run post processing
+    #
+    # masks = mask_generator.generate(image)
+    # print("Length of 'masks' is", len(masks))
+    predictor = SamPredictor(sam)
+    predictor.set_image(image)
+    input_point = np.array([[150, 222]])
+    input_label = np.array([1])
 
-    masks = mask_generator.generate(image)
-    print("Length of 'masks' is", len(masks))
-    
-
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    show_points(input_point, input_label, plt.gca())
+    plt.axis('on')
     plt.show()
 
+    masks, scores, logits = predictor.predict(
+        point_coords=input_point,
+        point_labels=input_label,
+        multimask_output=True,
+    )
+
+    for i, (mask, score) in enumerate(zip(masks, scores)):
+        plt.figure(figsize=(10, 10))
+        plt.imshow(image)
+        show_mask(mask, plt.gca())
+        show_points(input_point, input_label, plt.gca())
+        plt.title(f"Mask {i + 1}, Score: {score:.3f}", fontsize=18)
+        plt.axis('off')
+        plt.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
